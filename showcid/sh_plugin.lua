1a2faea577f0e1aa44eb32451777e5c866c1d70e
Biscorn <alexandre.thomas@audencia.com>
2024-08-02 16:28:59 +0200

    Update sh_plugin.lua

--- showcid/sh_plugin.lua
+++ showcid/sh_plugin.lua
@@ -5,7 +5,11 @@ PLUGIN.author = "Hern"
 PLUGIN.description = "Print the CID of the character in the chat."
 
 ix.config.Add("distanceRecognition", 200, "Default recognition distance.", nil, {
-	category = "chat"
+	category = "CID command"
+})
+
+ix.config.Add("shouldRecognize", true, "Wether or not players will recognize the one who has used the command.", nil, {
+	category = "CID command"
 })
 
 ix.command.Add("ShowCID", {
@@ -14,7 +18,7 @@ ix.command.Add("ShowCID", {
 
         if !client:Alive() then return end
 
-		local cid = client:GetCharacter():GetInventory():HasItem("cid")
+	local cid = client:GetCharacter():GetInventory():HasItem("cid")
 
         if !cid then --if the character doesn't have a cid on him and the players runs the command on his current character
             ix.chat.Send(client, "me", L("noCIDSentence"))
@@ -25,11 +29,13 @@ ix.command.Add("ShowCID", {
 
         ix.chat.Send(client, "me", "displays his CID card and says, " .. client:GetCharacter():GetName() .. ", Citizen ID: " .. "#" .. cid:GetData("id") .. ".")
 
-        for _, v in ipairs(ents.FindInSphere(client:GetPos(), ix.config.Get("distanceRecognition", 200))) do --so everyone around recognize the character
-            if v:IsPlayer() and v != client then
-                character:Recognize(v:GetCharacter())
-            end
-        end
+	if ix.config.Get("shouldRecognize") then 
+	        for _, v in ipairs(ents.FindInSphere(client:GetPos(), ix.config.Get("distanceRecognition", 200))) do --so everyone around recognize the character
+	            if v:IsPlayer() and v != client then
+	                character:Recognize(v:GetCharacter())
+	            end
+	        end
+	end
 
         local cidNameData = "CID Name : " .. cid:GetData("name") .. " | " .. "CID : " .. cid:GetData("id") .. " | " .. "ITEM ID : " .. cid.id
 
