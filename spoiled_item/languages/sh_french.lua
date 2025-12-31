a7e6c86a96eff7bbe14decee8cde00749ee70940
Biscorn <104092112+Biscorn@users.noreply.github.com>
2025-12-31 11:23:45 +0100

    add french trad for spoiled item plugin

---
+++ spoiled_item/languages/sh_french.lua
@@ -0,0 +1,6 @@
+LANGUAGE = {
+    consumeDangerousSpoiled = "Consommer un produit périmé est dangereux pour la santé",
+    consumeTodaySpoiled = "A consommer aujourd'hui.",
+    consumptionLeftSpoiled = "A consommer sous %s jour(s).",
+    unknownSpoiled = "Inconnu"
+}
